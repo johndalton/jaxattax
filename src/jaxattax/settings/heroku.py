857d9a3912cc23efdca4728e1a9941eeea17c334
John Dalton <john@johndalton.info>
2021-04-06 17:16:14 +1000

    Adding some logging config

--- src/jaxattax/settings/heroku.py
+++ src/jaxattax/settings/heroku.py
@@ -3,3 +3,25 @@ import django_heroku
 from .base import *
 
 django_heroku.settings(locals())
+
+# From: https://docs.djangoproject.com/en/3.1/topics/logging/
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
