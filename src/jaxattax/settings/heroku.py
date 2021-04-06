c74b061cbac965291c75b86965cc3a235b52668f
Tim Heap <tim@timheap.me>
2021-04-06 22:13:44 +1000

    Maybe need to set the credentials explicitly?

--- src/jaxattax/settings/heroku.py
+++ src/jaxattax/settings/heroku.py
@@ -29,8 +29,9 @@ LOGGING = {
 }
 
 
-# AWS credentials are pulled from environment variables automatically by boto3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'jaxewin-media'
 AWS_S3_REGION_NAME = 'ap-southeast-2'
 AWS_S3_FILE_OVERWRITE = False
